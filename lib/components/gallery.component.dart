a05d7df51fd0a98f0ec045fc271d6fee961ad1d7
3sam3 <iamdap91@gmail.com>
2022-11-27 01:55:51 +0900

    feat: gallery vertical drag end event added

--- lib/components/gallery.component.dart
+++ lib/components/gallery.component.dart
@@ -19,30 +19,35 @@ class Gallery extends StatefulWidget {
 class _GalleryState extends State<Gallery> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: PhotoViewGallery.builder(
-      pageController: PageController(initialPage: widget.initialPage),
-      scrollPhysics: const BouncingScrollPhysics(),
-      builder: (BuildContext context, int index) {
-        return PhotoViewGalleryPageOptions(
-          imageProvider: NetworkImage(widget.images[index]),
-          initialScale: PhotoViewComputedScale.contained * 0.8,
-          heroAttributes: PhotoViewHeroAttributes(tag: '$index'),
-        );
+    return GestureDetector(
+      onVerticalDragEnd: (details) {
+        Navigator.of(context).pop();
       },
-      itemCount: widget.images.length,
-      loadingBuilder: (context, event) => Center(
-        child: Container(
-          width: 20.0,
-          height: 20.0,
-          child: CircularProgressIndicator(
-            value: event == null
-                ? 0
-                : event.cumulativeBytesLoaded /
-                    (event.expectedTotalBytes ?? 0).toDouble(),
+      child: Container(
+          child: PhotoViewGallery.builder(
+        pageController: PageController(initialPage: widget.initialPage),
+        scrollPhysics: const BouncingScrollPhysics(),
+        builder: (BuildContext context, int index) {
+          return PhotoViewGalleryPageOptions(
+            imageProvider: NetworkImage(widget.images[index]),
+            initialScale: PhotoViewComputedScale.contained * 0.8,
+            heroAttributes: PhotoViewHeroAttributes(tag: '$index'),
+          );
+        },
+        itemCount: widget.images.length,
+        loadingBuilder: (context, event) => Center(
+          child: Container(
+            width: 20.0,
+            height: 20.0,
+            child: CircularProgressIndicator(
+              value: event == null
+                  ? 0
+                  : event.cumulativeBytesLoaded /
+                      (event.expectedTotalBytes ?? 0).toDouble(),
+            ),
           ),
         ),
-      ),
-    ));
+      )),
+    );
   }
 }
