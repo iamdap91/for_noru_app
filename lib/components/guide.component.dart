c82a9ee87f948c0cd8bcddffe87c35885e0955a0
3sam3 <iamdap91@gmail.com>
2022-11-29 21:59:35 +0900

    feat: tutorial

--- lib/components/guide.component.dart
+++ lib/components/guide.component.dart
@@ -20,7 +20,7 @@ class GuideState extends State<Guide> {
         title: '검색 필터',
         description: '목록에서 필터를 통해 우리 아이와 같이 갈 수 있는 장소들을 검색해보세요!',
         pathImage: 'assets/eraser.png',
-        backgroundColor: Color(0xfff5a623),
+        // backgroundColor: Color(0xfff5a623),
       ),
     );
     listContentConfig.add(
@@ -28,14 +28,15 @@ class GuideState extends State<Guide> {
         title: '투표',
         description: '투표를 통해 앱을 개선할 수 있어요!',
         pathImage: 'assets/pencil.png',
-        backgroundColor: Color(0xff203152),
+        // backgroundColor: Color(0xff203152),
       ),
     );
     listContentConfig.add(
       const ContentConfig(
         title: 'READY TO GO',
         pathImage: 'assets/ruler.png',
-        backgroundColor: Color(0xff9932CC),
+        description: '우측 상단의 버튼을 통해 안내를 다시 보실 수 있어요!',
+        // backgroundColor: Color(0xff9932CC),
       ),
     );
   }
