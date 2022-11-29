09f352ca367967f5bccf2e7db3ddceb4d3ecd9e6
3sam3 <iamdap91@gmail.com>
2022-11-30 03:36:07 +0900

    feat: applied filter bar function

--- lib/components/filter-bar.component.dart
+++ lib/components/filter-bar.component.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
-import '../utils/get-position.dart';
+import 'package:for_noru_app/stores/list-view-store.dart';
+import 'package:provider/provider.dart';
 
 List<String> categories = ['전체', '카페', '음식점'];
 final List<bool> selectedCategories = <bool>[true, false, false];
@@ -13,17 +13,6 @@ class FilterBar extends StatefulWidget {
 }
 
 class _FilterBarState extends State<FilterBar> {
-  figurePosition() async {
-    var position = await getPosition();
-    print(position);
-    return position;
-  }
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -33,15 +22,11 @@ class _FilterBarState extends State<FilterBar> {
         borderRadius: const BorderRadius.all(Radius.circular(8)),
         direction: Axis.horizontal,
         onPressed: (int index) {
-          setState(() {
-            for (int i = 0; i < selectedCategories.length; i++) {
-              selectedCategories[i] = i == index;
-            }
-          });
+          context.read<ListViewStore>().selectCategory(index);
         },
         constraints: BoxConstraints(minHeight: 40.0, minWidth: 80.0),
-        isSelected: selectedCategories,
-        children: categories.map((item) {
+        isSelected: context.watch<ListViewStore>().categorySelections,
+        children: context.watch<ListViewStore>().categories.map((item) {
           return Container(
             child: Text(item),
             padding: EdgeInsets.all(5),
--- lib/stores/list-view-store.dart
+++ lib/stores/list-view-store.dart
@@ -5,23 +5,30 @@ import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart';
 
 class ListViewStore extends ChangeNotifier {
-  final List<String> categories = ['전체', '카페', '음식점'];
-  final List<bool> categorySelections = [true, false, false];
+  final List<String> categories = ['카페', '음식점'];
+  final List<bool> categorySelections = [true, false];
   int currentIndex = 0;
 
   List<dynamic> listItems = [];
 
   void selectCategory(int index) {
     for (int i = 0; i < categories.length; i++) {
-      categorySelections[i] = i == index;
+      if (i == index) {
+        categorySelections[i] = i == index;
+        currentIndex = index;
+      }
     }
+    searchRequest();
     notifyListeners();
   }
 
   Future<void> searchRequest() async {
     Position position = await getPosition();
-    Response response = await get(Uri.parse(
-        'http://192.168.0.101:3333/api/search?lat=${position.latitude}&lon=${position.longitude}'));
+    String url = 'http://192.168.0.101:3333/api/search';
+    String query =
+        'lat=${position.latitude}&lon=${position.longitude}&category=${categories[currentIndex]}';
+
+    Response response = await get(Uri.parse('$url?$query'));
     var result = jsonDecode(response.body);
 
     listItems = result['hits'];
